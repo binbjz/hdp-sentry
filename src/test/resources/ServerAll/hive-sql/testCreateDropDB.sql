196862b497e160407bf923d114631a86bcc4a0cf
binbjz <binbjz@gmail.com>
2017-09-29 10:36:20 +0800

    add sql

--- src/test/resources/ServerAll/hive-sql/testCreateDropDB.sql
+++ src/test/resources/ServerAll/hive-sql/testCreateDropDB.sql
@@ -1,6 +1,5 @@
-SHOW DATABASES;
---DROP DATABASE IF EXISTS test_db CASCADE;
---CREATE DATABASE test_db WITH DBPROPERTIES ('creator' = 'hadoop-QA', 'date' = '2017-10-02');
---SHOW DATABASES LIKE 'test_db';
---DROP DATABASE test_db;
---SHOW DATABASES LIKE 'test_db';
+DROP DATABASE IF EXISTS test_db CASCADE;
+CREATE DATABASE test_db WITH DBPROPERTIES ('creator' = 'hadoop-QA', 'date' = '2017-10-02');
+SHOW DATABASES LIKE 'test_db';
+DROP DATABASE test_db;
+SHOW DATABASES LIKE 'test_db';
