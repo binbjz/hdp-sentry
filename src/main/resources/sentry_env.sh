ec8a4acee230a180a2b30d443dc8f72a59fce1e6
binbjz <binbjz@gmail.com>
2017-10-06 23:14:52 +0800

    add test files

--- src/main/resources/sentry_env.sh
+++ src/main/resources/sentry_env.sh
@@ -26,25 +26,25 @@ source /opt/meituan/hadoop/bin/hadoop_user_login.sh $LOGIN_USER
 [ $# -ne $ARGS ] && echo "Usage: `basename $BASH_SOURCE` (setup|clean|check) (privilege)" && exit $BAD_PARAMS
 
 # Select the corresponding privilege
-declare -A sentry_privilege
+declare -A sentry_privileges
 
-sentry_privilege[ServerAll]="server=server1->action=all"
-sentry_privilege[ServerCreate]="server=server1->action=create"
-sentry_privilege[ServerSelect]="server=server1->action=select"
-sentry_privilege[ServerInsert]="server=server1->action=insert"
+sentry_privileges[ServerAll]="server=server1->action=all"
+sentry_privileges[ServerCreate]="server=server1->action=create"
+sentry_privileges[ServerSelect]="server=server1->action=select"
+sentry_privileges[ServerInsert]="server=server1->action=insert"
 
-sentry_privilege[DBCreate]="server=server1->db=test_db2->action=create"
-sentry_privilege[ColumnAll]="server=server1->db=db4create->action=all,server=server1->db=db4drop->action=all,server=server1->db=db4drop_cascade->action=all,server=server1->db=db4alter->action=all,server=server1->db=test_db2->action=all,server=server1->db=db4tbl->action=all"
-sentry_privilege[DBInsert]="server=server1->db=test_db->action=insert,server=server1->db=db4show->action=insert,server=server1->db=db4drop->action=insert"
-sentry_privilege[DBSelect]="server=server1->db=test_db->action=select"
+sentry_privileges[DBCreate]="server=server1->db=test_db2->action=create"
+sentry_privileges[ColumnAll]="server=server1->db=db4create->action=all,server=server1->db=db4drop->action=all,server=server1->db=db4drop_cascade->action=all,server=server1->db=db4alter->action=all,server=server1->db=test_db2->action=all,server=server1->db=db4tbl->action=all"
+sentry_privileges[DBInsert]="server=server1->db=test_db->action=insert,server=server1->db=db4show->action=insert,server=server1->db=db4drop->action=insert"
+sentry_privileges[DBSelect]="server=server1->db=test_db->action=select"
 
 
 if [[ "$2" == "ServerAll" ]]; then
-    privilege=`awk 'BEGIN{FS=","}{for(i=1;i<=NF;i++)print $i}' <<< "${sentry_privilege[ServerAll]}"`
+    privileges=`awk 'BEGIN{FS=","}{for(i=1;i<=NF;i++)print $i}' <<< "${sentry_privileges[ServerAll]}"`
 elif [[ "$2" == "DBCreate" ]]; then
-    privilege=`awk 'BEGIN{FS=","}{for(i=1;i<=NF;i++)print $i}' <<< "${sentry_privilege[DBCreate]}"`
+    privileges=`awk 'BEGIN{FS=","}{for(i=1;i<=NF;i++)print $i}' <<< "${sentry_privileges[DBCreate]}"`
 elif [[ "$2" == "ColumnAll" ]]; then
-    privilege=`awk 'BEGIN{FS=","}{for(i=1;i<=NF;i++)print $i}' <<< "${sentry_privilege[ColumnAll]}"`
+    privileges=`awk 'BEGIN{FS=","}{for(i=1;i<=NF;i++)print $i}' <<< "${sentry_privileges[ColumnAll]}"`
 elif [[ "$2" == "List" ]]; then # For show privilege to specify $2 with List
     : ${privilege:="placeholder"}
 else
@@ -52,7 +52,7 @@ else
     exit $NOPRI
 fi
 
-for priv in $privilege; do
+for priv in $privileges; do
     # Select the corresponding action
     case "$1" in
     "setup")
--- src/test/java/ServerAll.java
+++ src/test/java/ServerAll.java
@@ -25,22 +25,23 @@ public class ServerAll {
         System.out.println("setUp......");
 
         String sentrySh = SentryConstant.jSrc + File.separator + SentryConstant.sentry_sh + " setup " + className;
-        String[] sentryCmd = {"/bin/bash", "-c", "source " + sentrySh};
+        String[] sentryCmd = {"/bin/bash", "-c", "\"source " + sentrySh + "\""};
         System.out.println(UtilTool.arrToStr(sentryCmd));
         System.out.println("exit code:\n" + UtilTool.execCommand(sentryCmd).get(0));
 
         String hiveSh = SentryConstant.jSrc + File.separator + SentryConstant.hive_sh + " proxy_user";
-        String[] hiveCmd = {"/bin/bash", "-c", "source " + hiveSh};
+        String[] hiveCmd = {"/bin/bash", "-c", "\"source " + hiveSh + "\""};
         System.out.println(UtilTool.arrToStr(hiveCmd));
         System.out.println("exit code:\n" + UtilTool.execCommand(hiveCmd).get(0));
     }
 
 
     @Test(dataProvider = "DataProvider_ServerAll")
-    public void testServerAll(String id, String tag, String desc, String sqlType, String sqlFile, String resultType, String resultFile) {
+    public void testServerAll(String id, String tag, String desc, String sqlType, String sqlFile, String
+            resultType, String resultFile) {
         System.out.println("running testcase: " + id);
         String hiveSql = SentryConstant.hiveExec + " -f " + hiveSqlPath + File.separator + sqlFile;
-        String[] sqlCmd = {"/bin/bash", "-c", hiveSql};
+        String[] sqlCmd = {"/bin/bash", "-c", "\"" + hiveSql + "\""};
         System.out.println(UtilTool.arrToStr(sqlCmd));
 
         Map map = UtilTool.execCommand(sqlCmd);
