c693b810c9d639852fae93e466b09253133e164a
binbjz <binbjz@gmail.com>
2017-10-09 13:15:11 +0800

    add DBInsert

--- src/test/resources/DBInsert/hive-sql/testCreateDropShowDB.sql
+++ src/test/resources/DBInsert/hive-sql/testCreateDropShowDB.sql
@@ -1,5 +1,9 @@
+CREATE DATABASE db4show WITH DBPROPERTIES ('creator' = 'hadoop-QA', 'date' = '2017-10-02');
+
 USE default;
 CREATE DATABASE db2create WITH DBPROPERTIES ('creator' = 'hadoop-QA', 'date' = '2017-10-02');
 DESCRIBE DATABASE db4show;
 DROP DATABASE db4show;
 SHOW DATABASES;
+
+DROP DATABASE db4show;
--- src/test/resources/DBInsert/hive-sql/testDropDBCascade.sql
+++ src/test/resources/DBInsert/hive-sql/testDropDBCascade.sql
@@ -1,4 +1,3 @@
-DROP DATABASE IF EXISTS test_db2 CASCADE;
 CREATE DATABASE test_db2 WITH DBPROPERTIES ('creator' = 'hadoop-QA', 'date' = '2017-10-02');
 CREATE TABLE test_db2.tbl4drop (id INT, val STRING);
 CREATE VIEW test_db2.view4drop AS SELECT id, val FROM test_db2.tbl4drop;
@@ -6,3 +5,5 @@ CREATE VIEW test_db2.view4drop AS SELECT id, val FROM test_db2.tbl4drop;
 USE default;
 DROP DATABASE test_db2 CASCADE;
 SHOW DATABASES;
+
+DROP DATABASE test_db2 CASCADE;
