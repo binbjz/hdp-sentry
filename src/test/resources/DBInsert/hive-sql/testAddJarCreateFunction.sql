815914d4d195c23f02e390c4a0ceeee0a39f7021
binbjz <binbjz@gmail.com>
2017-10-09 16:52:23 +0800

    add DBInsert

--- src/test/resources/DBInsert/hive-sql/testAddJarCreateFunction.sql
+++ src/test/resources/DBInsert/hive-sql/testAddJarCreateFunction.sql
@@ -4,13 +4,14 @@ USE db4func;
 CREATE TABLE teacher (name STRING);
 
 --Execution:
-ADD JAR /opt/meituan/qa_test/testfile/hive_qa_udf.jar;
+set FILEPATH=/opt/meituan/qa_test/sentry-test/src/test/resources/hive-data;
+ADD JAR '${hiveconf:FILEPATH}/hive_qa_udf.jar';
 LIST JARS;
 CREATE FUNCTION qa_lower AS 'com.example.hive.udf.LowerCase';
 INSERT INTO teacher VALUES ('TEACHER QA');
 SELECT qa_lower(name) as name FROM teacher;
 SHOW FUNCTIONS LIKE 'qa_lower';
-DELETE JAR /opt/meituan/qa_test/testfile/hive_qa_udf.jar;
+DELETE JAR '${hiveconf:FILEPATH}/hive_qa_udf.jar';
 
 --Cleanup:
 DROP TABLE teacher;
