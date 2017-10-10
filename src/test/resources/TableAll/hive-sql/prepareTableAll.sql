6d0b9a137fd61476cab07bdafed79131034537ff
binbjz <binbjz@gmail.com>
2017-10-10 20:04:30 +0800

    Add results Table all

--- src/test/resources/TableAll/hive-sql/prepareTableAll.sql
+++ src/test/resources/TableAll/hive-sql/prepareTableAll.sql
@@ -1,71 +1,56 @@
 CREATE DATABASE unaccessdb WITH DBPROPERTIES ('creator' = 'hadoop-QA', 'date' = '2017-10-02');
-
-CREATE DATABASE testDB WITH DBPROPERTIES ('creator' = 'hadoop-QA', 'date' = '2017-10-02');
-
 CREATE DATABASE testDB WITH DBPROPERTIES ('creator' = 'hadoop-QA', 'date' = '2017-10-02');
 CREATE TABLE testDB.tbl4drop (col1 TINYINT, col2 SMALLINT);
 CREATE VIEW testDB.view4drop AS SELECT col1, col2 FROM testDB.tbl4drop;
 CREATE TABLE testDB.tbl4drop_no_r (col1 TINYINT, col2 SMALLINT);
 CREATE VIEW testDB.view4drop_no_r AS SELECT col1, col2 FROM testDB.tbl4drop_no_r;
-
-CREATE DATABASE testDB WITH DBPROPERTIES ('creator' = 'hadoop-QA', 'date' = '2017-10-02');
 CREATE TABLE testDB.tbl4alter (col1 TINYINT, col2 SMALLINT, col3 INT, col4 BIGINT, col5 BOOLEAN, col6 FLOAT, col7 DOUBLE, col8 STRING, col9 TIMESTAMP);
-
-CREATE DATABASE testDB;
- CREATE TABLE IF NOT EXISTS testDB.log_messages (hms INT, severity STRING, server STRING, process_id INT, message STRING)
- PARTITIONED BY (year INT, month INT, day INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
- CREATE EXTERNAL TABLE IF NOT EXISTS testDB.log_messages_external (hms INT, severity STRING, server STRING, process_id INT, message STRING) PARTITIONED BY (year INT, month INT, day INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
-
-CREATE DATABASE testDB;
 CREATE TABLE IF NOT EXISTS testDB.log_messages (hms INT, severity STRING, server STRING, process_id INT, message STRING)
 PARTITIONED BY (year INT, month INT, day INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
-
-CREATE DATABASE testDB;
+CREATE EXTERNAL TABLE IF NOT EXISTS testDB.log_messages_external (hms INT, severity STRING, server STRING, process_id INT, message STRING) PARTITIONED BY (year INT, month INT, day INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
+CREATE TABLE IF NOT EXISTS testDB.log_messages2 (hms INT, severity STRING, server STRING, process_id INT, message STRING)
+PARTITIONED BY (year INT, month INT, day INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
 CREATE TABLE testDB.supply (id INT, part STRING, quantity INT)  PARTITIONED BY (day INT);
+CREATE TABLE testDB.collecttest (str STRING, countVal INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY '&' LINES TERMINATED BY '10';
+CREATE TABLE testDB.session_test (
+  st_user_id STRING
+ ,st_pageview_id STRING
+ ,st_page_url STRING
+ ,st_referrer_url STRING
+ ,st_timestamp DOUBLE);
 
-CREATE DATABASE testDB;
-CREATE TABLE testDB.session_test ( st_user_id STRING,st_pageview_id STRING,st_page_url STRING,st_referrer_url STRING,st_timestamp DOUBLE);
+CREATE TABLE testDB.sessionization_step_one_origins (
+  ssoo_user_id STRING
+ ,ssoo_pageview_id STRING
+ ,ssoo_timestamp DOUBLE);
 
-CREATE TABLE testDB.sessionization_step_one_origins ( ssoo_user_id STRING,ssoo_pageview_id STRING,ssoo_timestamp DOUBLE);
-
-CREATE DATABASE testDB;
 CREATE TABLE testDB.partition_table001 (name STRING, ip STRING)
 PARTITIONED BY (dt STRING, ht STRING)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY "\t";
 CREATE TABLE IF NOT EXISTS testDB.partition_table002 LIKE testDB.partition_table001;
 
-CREATE DATABASE testDB;
 CREATE TABLE testDB.table001 (name STRING, ip STRING)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY "\t";
 CREATE TABLE IF NOT EXISTS testDB.table002 LIKE testDB.table001;
 
-CREATE DATABASE testDB;
-CREATE TABLE teacher (name STRING);
-INSERT INTO teacher VALUES ('TEACHER QA');
+CREATE TABLE testDB.teacher (name STRING);
+INSERT INTO testDB.teacher VALUES ('TEACHER QA');
 
-CREATE DATABASE testDB;
-CREATE TABLE teacher (name STRING);
-INSERT INTO teacher VALUES ('TEACHER QA');
-
-CREATE DATABASE testDB;
 CREATE TABLE testDB.whoyouare(who string);
 
-CREATE DATABASE testDB;
 CREATE TABLE testDB.test_serde (c0 string, c1 string, c2 string) ROW FORMAT SERDE 'org.apache.hadoop.hive.contrib.serde2.RegexSerDe'
 WITH SERDEPROPERTIES ('input.regex' = 'bduid\\[(.*)\\]uid\\[(\\d+)\\]uname\\[(.*)\\]', 'output.format.string' = '%1$s\t%2$s') STORED AS TEXTFILE;
 
-CREATE DATABASE testDB;
 CREATE TABLE testDB.test_partition_serde(c0 string, c1 string, c2 string) PARTITIONED BY (col10 STRING, col20 STRING)
 ROW FORMAT SERDE 'org.apache.hadoop.hive.contrib.serde2.RegexSerDe'
 WITH SERDEPROPERTIES ('input.regex' = 'bduid\\[(.*)\\]uid\\[(\\d+)\\]uname\\[(.*)\\]', 'output.format.string' = '%1$s\t%2$s') STORED AS TEXTFILE;
 
-CREATE DATABASE testDB;
 CREATE TABLE testDB.staged_employees (
-   name STRING
-  ,salary FLOAT
-  ,subordinates ARRAY<STRING>
-  ,deductions MAP<STRING, FLOAT>
-  ,address STRUCT<street:STRING, city:STRING, state:STRING, zip:INT>
+ name STRING
+,salary FLOAT
+,subordinates ARRAY<STRING>
+,deductions MAP<STRING, FLOAT>
+,address STRUCT<street:STRING, city:STRING, state:STRING, zip:INT>
 ) PARTITIONED BY (country STRING, state STRING)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 COLLECTION ITEMS TERMINATED BY '|'
@@ -73,53 +58,59 @@ MAP KEYS TERMINATED BY '='
 LINES TERMINATED BY '\n' STORED AS TEXTFILE;
 
 CREATE TABLE testDB.employees (
-   name STRING
-  ,salary FLOAT
-  ,subordinates ARRAY<STRING>
-  ,deductions MAP<STRING, FLOAT>
-  ,address STRUCT<street:STRING, city:STRING, state:STRING, zip:INT>
+ name STRING
+,salary FLOAT
+,subordinates ARRAY<STRING>
+,deductions MAP<STRING, FLOAT>
+,address STRUCT<street:STRING, city:STRING, state:STRING, zip:INT>
+) PARTITIONED BY (country STRING, state STRING)
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
+COLLECTION ITEMS TERMINATED BY '|'
+MAP KEYS TERMINATED BY '='
+LINES TERMINATED BY '\n' STORED AS TEXTFILE;
+
+CREATE TABLE testDB.employees02 (
+ name STRING
+,salary FLOAT
+,subordinates ARRAY<STRING>
+,deductions MAP<STRING, FLOAT>
+,address STRUCT<street:STRING, city:STRING, state:STRING, zip:INT>
 ) PARTITIONED BY (country STRING, state STRING)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 COLLECTION ITEMS TERMINATED BY '|'
 MAP KEYS TERMINATED BY '='
 LINES TERMINATED BY '\n' STORED AS TEXTFILE;
 
+CREATE TABLE testDB.staged_employees02 (
+ name STRING
+,salary FLOAT
+,subordinates ARRAY<STRING>
+,deductions MAP<STRING, FLOAT>
+,address STRUCT<street:STRING, city:STRING, state:STRING, zip:INT>
+) PARTITIONED BY (country STRING, state STRING)
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+COLLECTION ITEMS TERMINATED BY '|'
+MAP KEYS TERMINATED BY '='
+LINES TERMINATED BY '\n' STORED AS TEXTFILE;
 
-CREATE DATABASE testDB;
 CREATE TABLE IF NOT EXISTS testDB.test_enable_disable1 (hms INT, severity STRING, server STRING, process_id INT, message STRING)
 PARTITIONED BY (year INT, month INT, day INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
 
 CREATE TABLE IF NOT EXISTS testDB.test_enable_disable2 (hms INT, severity STRING, server STRING, process_id INT, message STRING)
 PARTITIONED BY (year INT, month INT, day INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
 
-
-CREATE DATABASE testDB;
 CREATE TABLE IF NOT EXISTS testDB.test_enable_disable (hms INT, severity STRING, server STRING, process_id INT, message STRING)
 PARTITIONED BY (year INT, month INT, day INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
 
-CREATE DATABASE testDB;
 CREATE TABLE testDB.test_msck (id INT, val STRING) PARTITIONED BY(month INT);
 
-CREATE DATABASE testDB;
 CREATE TABLE IF NOT EXISTS testDB.employees_props (
-  name  STRING COMMENT 'Employee name'
- ,salary  FLOAT  COMMENT'Employee salary'
- ,subordinates ARRAY<STRING> COMMENT 'Names of subordinates'
- ,deductions  MAP<STRING, FLOAT> COMMENT  'Keys are deductions names, values are percentages'
- ,address  STRUCT<street:STRING,city:STRING, state:STRING, zip:INT> COMMENT 'Home address')
+ name  STRING COMMENT 'Employee name'
+,salary  FLOAT  COMMENT'Employee salary'
+,subordinates ARRAY<STRING> COMMENT 'Names of subordinates'
+,deductions  MAP<STRING, FLOAT> COMMENT  'Keys are deductions names, values are percentages'
+,address  STRUCT<street:STRING,city:STRING, state:STRING, zip:INT> COMMENT 'Home address')
 COMMENT 'Description of the table'
 PARTITIONED BY (country STRING, state STRING)
 LOCATION '/user/hive/warehouse/testDB.db/employees_props'
 TBLPROPERTIES ('creator'='HADOOP-QA','created_at'='2017-9-10 10:00:00', 'notes'='test show tblproperties');
-
-CREATE DATABASE testDB;
-
-CREATE DATABASE testDB;
-USE testDB;
-CREATE TABLE employees (
-  name STRING
- ,salary FLOAT
- ,subordinates ARRAY<STRING>
- ,deductions MAP<STRING, FLOAT>
- ,address STRUCT<street:STRING, city:STRING, state:STRING, zip:INT>
-) PARTITIONED BY (country STRING, state STRING);
