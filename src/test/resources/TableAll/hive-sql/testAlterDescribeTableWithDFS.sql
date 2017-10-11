c54116aec628ce989fbeeb32b78b8ebef3884c1e
binbjz <binbjz@gmail.com>
2017-10-11 17:03:36 +0800

    update test cases

--- src/test/resources/TableAll/hive-sql/testAlterDescribeTableWithDFS.sql
+++ src/test/resources/TableAll/hive-sql/testAlterDescribeTableWithDFS.sql
@@ -13,7 +13,13 @@ DESC FORMATTED testdb.log_messages2;
 --获取partition的真实hdfs路径
 DESC FORMATTED testdb.log_messages2 partition (year = 2011, month = 1, day =1);
 DESC FORMATTED testdb.log_messages2 partition (year = 2011, month = 1, day =2);
+
+dfs -mkdir /user/hive/warehouse/testdb.db/log_messages2;
+
+ALTER TABLE testdb.log_messages2 SET LOCATION 'viewfs://hadoop-meituan-test/user/hive/warehouse/testdb.db/log_messages_new2';
+DESC FORMATTED testdb.log_messages2;
 --使用hadoop fs -rmr 命令删除分区的HDFS副本：
 dfs -rm -r /user/hive/warehouse/testdb.db/log_messages2/year=2011/month=1/day=1;
+
 DROP TABLE testdb.log_messages2;
 dfs -rm -r /user/hive/warehouse/testdb.db/log_messages_new2;
