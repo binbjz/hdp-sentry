e8e355d7c7bbad7b2faebc41892c6d4c97a16cd4
binbjz <binbjz@gmail.com>
2017-10-12 15:35:13 +0800

    update test cases

--- src/test/resources/ServerSelect/hive-sql/testAlterDescribeTable01.sql
+++ src/test/resources/ServerSelect/hive-sql/testAlterDescribeTable01.sql
@@ -5,9 +5,8 @@ DESCRIBE test_tbl;
 ALTER TABLE test_tbl REPLACE COLUMNS (new_col1 INT COMMENT 'new column 1' , new_col2 STRING COMMENT 'new column 2', new_col3 STRING COMMENT 'new column3');
 SHOW CREATE TABLE test_tbl;
 
-ALTER TABLE test_tbl CHANGE new_col1 new_col1to2 INT COMMENT "put column1 to position 2nd" AFTER new_col2;
-ALTER TABLE test_tbl CHANGE new_col3 new_col3to1 INT COMMENT "put last column to position 1st" FIRST;
---ALTER TABLE test_tbl CHANGE col1 new_col1to2 INT COMMENT "put column1 to position 2nd" AFTER col2;
+ALTER TABLE test_tbl CHANGE col1 col1to2 INT COMMENT "put column1 to position 2nd" AFTER col2;
+ALTER TABLE test_tbl CHANGE col3 col3to1 INT COMMENT "put last column to position 1st" FIRST;
 
 DESCRIBE test_tbl;
 ALTER TABLE test_tbl RENAME TO test_tbl_new;
