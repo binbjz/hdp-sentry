c8c5ad5e3b08bc20f8db7d5796aac7397165c6f1
binbjz <binbjz@gmail.com>
2017-10-12 16:04:59 +0800

    update test cases

--- src/test/resources/ServerSelect/hive-sql/testAlterTableEnableDisable.sql
+++ src/test/resources/ServerSelect/hive-sql/testAlterTableEnableDisable.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 USE testdb;
 
 ALTER TABLE testdb.test_enable_disable1 ENABLE NO_DROP;
@@ -6,13 +5,6 @@ DROP TABLE testdb.test_enable_disable1;
 ALTER TABLE testdb.test_enable_disable1 DISABLE NO_DROP;
 DROP TABLE testdb.test_enable_disable1;
 
-=======
-ALTER TABLE testdb.test_enable_disable1 ENABLE NO_DROP;
-DROP TABLE testdb.test_enable_disable1;
-ALTER TABLE testdb.test_enable_disable1 DISABLE NO_DROP;
-DROP TABLE testdb.test_enable_disable1;
-
->>>>>>> b0450854b2f7ed1c2e1b2c8651afe9e2db333a37
 ALTER TABLE testdb.test_enable_disable2 ENABLE OFFLINE;
 DROP TABLE testdb.test_enable_disable2;
 ALTER TABLE testdb.test_enable_disable2 DISABLE OFFLINE;
