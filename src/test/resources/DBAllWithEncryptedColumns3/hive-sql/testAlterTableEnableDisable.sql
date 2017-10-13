de55f0cf6b7a191428344081d48cff792f0bb42f
binbjz <binbjz@gmail.com>
2017-10-13 20:37:16 +0800

    update test cases

--- src/test/resources/DBAllWithEncryptedColumns3/hive-sql/testAlterTableEnableDisable.sql
+++ src/test/resources/DBAllWithEncryptedColumns3/hive-sql/testAlterTableEnableDisable.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS encrypt_db4alter.log_messages (hms INT, severity STRING, server STRING, process_id INT, message STRING)
 PARTITIONED BY (year INT, month INT, day INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
 
+USE encrypt_db4alter;
 ALTER TABLE encrypt_db4alter.log_messages ENABLE NO_DROP;
 SELECT hms INT, severity, server, process_id, message FROM encrypt_db4alter.log_messages;
-DROP encrypt_db4alter.log_messages;
+DROP TABLE encrypt_db4alter.log_messages;
 ALTER TABLE encrypt_db4alter.log_messages DISABLE NO_DROP;
-DROP encrypt_db4alter.log_messages;
+DROP TABLE encrypt_db4alter.log_messages;
 
 CREATE TABLE IF NOT EXISTS encrypt_db4alter.log_messages (hms INT, severity STRING, server STRING, process_id INT, message STRING)
 PARTITIONED BY (year INT, month INT, day INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
--- src/test/resources/DBAllWithEncryptedColumns3/hive-sql/testShowColumnsAndTblProperties.sql
+++ src/test/resources/DBAllWithEncryptedColumns3/hive-sql/testShowColumnsAndTblProperties.sql
@@ -7,11 +7,11 @@ CREATE TABLE IF NOT EXISTS employees_props (
  ,address  STRUCT<street:STRING,city:STRING, state:STRING, zip:INT> COMMENT 'Home address')
 COMMENT 'Description of the table'
 PARTITIONED BY (country STRING, state STRING)
-LOCATION '/user/hive/warehouse/test_db.db/employees_props'
+LOCATION '/user/hive/warehouse/encrypt_db4props.db/employees_props'
 TBLPROPERTIES ('creator'='HADOOP-QA','created_at'='2017-9-10 10:00:00', 'notes'='test show tblproperties');
 
-SHOW TBLPROPERTIES employees_props;
-SHOW COLUMNS IN test_db.employees_props;
-SHOW COLUMNS FROM test_db.employees_props;
+SHOW TBLPROPERTIES encrypt_db4props.employees_props;
+SHOW COLUMNS IN encrypt_db4props.employees_props;
+SHOW COLUMNS FROM encrypt_db4props.employees_props;
 
-DROP TABLE employees_props;
+DROP TABLE encrypt_db4props.employees_props;
