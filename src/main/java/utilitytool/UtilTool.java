701b5d7955d38f718501a5ee3a135c62b4b07edd
binbjz <binbjz@gmail.com>
2017-10-16 15:16:23 +0800

    modify sentry files

--- src/main/java/utilitytool/UtilTool.java
+++ src/main/java/utilitytool/UtilTool.java
@@ -158,7 +158,7 @@ public class UtilTool {
      * example: "17/09/13 14:39:55 INFO xxxxxx" will be replaced with an empty string
      */
     public static String filterResults(String str) {
-        String REGEX = "(\\d{2}|\\d{4})(/|-)\\d{1,2}(/|-)\\d{1,2}\\s+\\d{2}:\\d{2}:\\d{2}";
+        String REGEX = "(\\d{2}|\\d{4})(/|-)\\d{1,2}(/|-)\\d{1,2}\\s+\\d{2}:\\d{2}(:\\d{2})?";
         String REGEX2 = "^(Time|CliDriver|Logging).*";
         String REPLACE = "";
         // String REGEX2 = "([a-zA-Z]+(:?)\\s+(!?))+";
