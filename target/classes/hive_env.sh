cb3ff57d1379ba6261c40233ecba62f8518c82b8
binbjz <binbjz@gmail.com>
2017-10-19 20:53:12 +0800

    modify sentry tool class

--- target/classes/hive_env.sh
+++ target/classes/hive_env.sh
@@ -13,13 +13,15 @@ HIVE_HOME=`readlink -f /opt/meituan/hive-1.2`
 
 
 # Check CLI parameter
-[ $# -ne $ARGS ] && echo "Usage: `basename $BASH_SOURCE` (keytab_auth|proxy_user|proxy_user_group) (super|normal)" && exit $BAD_PARAMS
+[ $# -ne $ARGS ] && echo "Usage: `basename $BASH_SOURCE` (keytab_auth|proxy_user|proxy_user_group|clean_proxy_user) (super|normal|hive)" && exit $BAD_PARAMS
 
 # set proxy user for specify privilege
 if [[ "$2" == "super" ]]; then
     PROXY_USER=mt_qa
 elif [[ "$2" == "normal" ]]; then
     PROXY_USER=hdp_qa
+elif [[ "$2" == "hive" ]]; then
+    :
 else
     echo "Please specify valid sentry privilege"
     exit $NOPRI
