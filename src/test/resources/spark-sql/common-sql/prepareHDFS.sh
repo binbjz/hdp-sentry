642c61a1786f48fd0ac909b04581d2828f6de630
binbjz <binbjz@gmail.com>
2017-11-09 20:20:16 +0800

    update test cases

--- src/test/resources/spark-sql/common-sql/prepareHDFS.sh
+++
@@ -1,7 +0,0 @@
-dfs -mkdir /user/hive/warehouse/testdb.db/tbl4setlocation_new;
-
-dfs -mkdir -p /user/hive/warehouse/testdb.db/tbl4setlocation_new/year=2011/month=1/day=1;
-
-/*创建两个分区目录*/
-dfs -mkdir /user/hive/warehouse/testdb.db/test_msck/month=201603;
-dfs -mkdir /user/hive/warehouse/testdb.db/test_msck/month=201604;
--- src/test/resources/spark-sql/common-sql/testMSCKRepairAnalyzeTableAddRenameDropShowPartition.scala
+++ src/test/resources/spark-sql/common-sql/testMSCKRepairAnalyzeTableAddRenameDropShowPartition.scala
@@ -7,12 +7,43 @@ spark.sql(test_sql).collect().foreach(println);
 val test_sql="SHOW PARTITIONS testdb.test_msck";
 spark.sql(test_sql).collect().foreach(println);
 
+
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+val fs=FileSystem.get(sc.hadoopConfiguration)
+
+
+/*创建两个分区目录
+dfs -mkdir /user/hive/warehouse/testdb.db/test_msck/month=201603;
+dfs -mkdir /user/hive/warehouse/testdb.db/test_msck/month=201604;
+*/
+val partitionPath="/user/hive/warehouse/testdb.db/test_msck/month=201603"
+if(fs.exists(new Path(partitionPath)))
+  fs.delete(new Path(partitionPath),true)
+if(!fs.exists(new Path(partitionPath)))
+  fs.mkdirs(new Path(partitionPath))
+
+
+val partitionPath="/user/hive/warehouse/testdb.db/test_msck/month=201604"
+if(fs.exists(new Path(partitionPath)))
+  fs.delete(new Path(partitionPath),true)
+if(!fs.exists(new Path(partitionPath)))
+  fs.mkdirs(new Path(partitionPath))
+
 /*使用MSCK修复分区*/
 val test_sql="MSCK REPAIR TABLE testdb.test_msck";
 spark.sql(test_sql).collect().foreach(println);
 /*再次查看，发现已经成功更新元信息*/
 val test_sql="SHOW PARTITIONS testdb.test_msck";
 spark.sql(test_sql).collect().foreach(println);
+
+val partitionPath="/user/hive/warehouse/testdb.db/test_msck/month=201603"
+if(fs.exists(new Path(outPutPath)))
+  fs.delete(new Path(outPutPath),true)
+
+val partitionPath="/user/hive/warehouse/testdb.db/test_msck/month=201604"
+if(fs.exists(new Path(outPutPath)))
+  fs.delete(new Path(outPutPath),true)
 
 
 /* testTableAnalyze */
@@ -69,3 +100,31 @@ val test_sql="SHOW PARTITIONS testdb.tbl4partition";
 spark.sql(test_sql).collect().foreach(println);
 
 System.exit(0);
+
+
+
+/*
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+val fs=FileSystem.get(sc.hadoopConfiguration)
+val outPutPath="/tmp/abc"
+if(fs.exists(new Path(outPutPath)))
+  fs.delete(new Path(outPutPath),true)
+if(!fs.exists(new Path(outPutPath)))
+  fs.mkdirs(new Path(outPutPath))
+*/
+/*
+val partitionPath="dfs -mkdir /user/hive/warehouse/testdb.db/test_msck/month=201604"
+val mkdir = (path: String) => {
+   val fs=FileSystem.get(sc.hadoopConfiguration)
+
+   if(fs.exists(new Path(outPutPath)))
+     fs.delete(new Path(outPutPath),true)
+   if(!fs.exists(new Path(outPutPath)))
+     fs.mkdirs(new Path(outPutPath))
+}
+spark.udf.register("mkdir", mkdir);
+val test_sql="SELECT id, square(id) as id_squared FROM testdb.tbl4udf";
+spark.sql(test_sql).collect().foreach(println);
+*/
+
