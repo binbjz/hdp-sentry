28808dcf8b628788e99a5589113e64f7e3cfa507
binbjz <binbjz@gmail.com>
2017-11-18 19:17:12 +0800

    update test cases

--- src/test/resources/spark-sql/GroupLogin-sql/testSampleTruncate.scala
+++ src/test/resources/spark-sql/GroupLogin-sql/testSampleTruncate.scala
@@ -7,5 +7,10 @@ spark.sql(test_sql).collect().foreach(println);
 val test_sql="SELECT * FROM mart_waimai.tbl4sample TABLESAMPLE(2 ROWS) s";
 spark.sql(test_sql).collect().foreach(println);
 
+val test_sql="TRUNCATE TABLE mart_waimai.tbl4sample";
+spark.sql(test_sql).collect().foreach(println);
+
+val test_sql="SELECT * FROM mart_waimai.tbl4sample";
+spark.sql(test_sql).collect().foreach(println);
 System.exit(0);
 
