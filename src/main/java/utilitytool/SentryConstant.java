a9628b943da05c960fed55f6cec356b719b6cee9
binbjz <binbjz@gmail.com>
2017-11-29 18:18:56 +0800

    sentry hive spark

--- src/main/java/utilitytool/SentryConstant.java
+++ src/main/java/utilitytool/SentryConstant.java
@@ -10,9 +10,13 @@ public class SentryConstant {
     public static final String tSrc = Paths.get("src" + File.separator + "test" + File.separator + "resources").toAbsolutePath().toString();
     public static final String sqlOutput = "sql-output";
     public static final String suffix_json = ".json";
+    public static final String hive_12 = "hive-1.2";
+    public static final String spark_21 = "spark-2.1";
+    public static final String spark_22 = "spark-2.2";
 
+    /*
     // Define hive executor vars. If run hive sql please comment spark env statements.
-    public static final String execSrc = File.separator + "opt" + File.separator + "meituan" + File.separator + "hive-1.2" + File.separator + "bin" + File.separator + "hive";
+    public static final String execSrc = File.separator + "opt" + File.separator + "meituan" + File.separator + hive_12 + File.separator + "bin" + File.separator + "hive";
     public static final String cmdExec = String.format("%s --hiveconf hive.cli.errors.ignore=true -f ", execSrc);
     public static final String sqlSrc = tSrc + File.separator + "hive-sql";
     public static final String commonSqlSrc = sqlSrc + File.separator + "common-sql";
@@ -20,10 +24,10 @@ public class SentryConstant {
     public static final String groupLoginSqlSrc = sqlSrc + File.separator + "GroupLogin-sql";
     public static final String stdOutput = "hive-output";
     public static final String jsonInput = "hive-json-input";
+    */
 
-    /*
     // Define spark executor vars. If run spark sql please comment hive env statements.
-    public static final String execSrc = File.separator + "opt" + File.separator + "meituan" + File.separator + "spark-2.2" + File.separator + "bin" + File.separator + "spark-shell";
+    public static final String execSrc = File.separator + "opt" + File.separator + "meituan" + File.separator + spark_21 + File.separator + "bin" + File.separator + "spark-shell";
     public static final String hiveDataSrc = tSrc + File.separator + "source-data";
     public static final String cmdExec = String.format("%s --master yarn --deploy-mode client --queue root.hadoop-hdp.etltest --jars %s -i ", execSrc, hiveDataSrc + File.separator + "hive_qa_udf.jar");
     public static final String sqlSrc = tSrc + File.separator + "spark-sql";
@@ -32,5 +36,4 @@ public class SentryConstant {
     public static final String groupLoginSqlSrc = sqlSrc + File.separator + "GroupLogin-sql";
     public static final String stdOutput = "spark-output";
     public static final String jsonInput = "spark-json-input";
-    */
 }
