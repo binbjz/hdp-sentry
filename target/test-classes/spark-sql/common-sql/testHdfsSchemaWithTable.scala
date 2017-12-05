2b71b09024b961fa1a24b4431aa8318b126c4016
binbjz <binbjz@gmail.com>
2017-12-05 13:22:45 +0800

    sentry hive spark

--- target/test-classes/spark-sql/common-sql/testHdfsSchemaWithTable.scala
+++ target/test-classes/spark-sql/common-sql/testHdfsSchemaWithTable.scala
@@ -2,10 +2,11 @@
 // This is used to implicitly convert an RDD to a DataFrame.
 
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
 import sqlContext.implicits._
 import org.apache.spark.sql.SaveMode
 
-val hdfsPath="/user/hive/warehouse/testdb.db/test_schema"
+val hdfsPath = "/user/hive/warehouse/testdb.db/test_schema"
 
 // Create a simple DataFrame, stored into a partition directory
 val df = sc.makeRDD(1 to 5).map(i => (i, i * 2)).toDF("single", "double")
@@ -24,14 +25,14 @@ df.printSchema()
 val df = spark.read.option("mergeSchema", "true").parquet(hdfsPath)
 df.printSchema()
 df.write.mode(SaveMode.Overwrite).saveAsTable("testdb.spark_merge_schema_true")
-val test_sql="SELECT * FROM testdb.spark_merge_schema_true ORDER BY 1";
+val test_sql = "SELECT * FROM testdb.spark_merge_schema_true ORDER BY 1";
 spark.sql(test_sql).collect().foreach(println);
 
 
 val df = spark.read.option("mergeSchema", "false").parquet(hdfsPath)
 df.printSchema()
 df.write.partitionBy("key").mode(SaveMode.Overwrite).saveAsTable("testdb.spark_merge_schema_false")
-val test_sql="SELECT * FROM testdb.spark_merge_schema_false ORDER BY 1";
+val test_sql = "SELECT * FROM testdb.spark_merge_schema_false ORDER BY 1";
 spark.sql(test_sql).collect().foreach(println);
 
 // The final schema consists of all 3 columns in the Parquet files together
@@ -45,17 +46,18 @@ spark.sql(test_sql).collect().foreach(println);
 
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.Path
-val fs=FileSystem.get(sc.hadoopConfiguration)
+
+val fs = FileSystem.get(sc.hadoopConfiguration)
 
 /* cleanup HDFS dir */
-if(fs.exists(new Path(hdfsPath)))
-  fs.delete(new Path(hdfsPath),true)
+if (fs.exists(new Path(hdfsPath)))
+  fs.delete(new Path(hdfsPath), true)
 
 
 /* cleanup HDFS dir */
-val csvPath="/user/hive/warehouse/testdb.db/datafile.csv"
-if(fs.exists(new Path(csvPath)))
-  fs.delete(new Path(csvPath),true)
+val csvPath = "/user/hive/warehouse/testdb.db/datafile.csv"
+if (fs.exists(new Path(csvPath)))
+  fs.delete(new Path(csvPath), true)
 /* RDD save to CSV file */
 val df = sc.makeRDD(1 to 5).map(i => (i, i * 2))
 df.saveAsTextFile(csvPath)
@@ -63,7 +65,7 @@ val input = sc.textFile(csvPath)
 input.first
 input.collect.foreach(println)
 /* cleanup HDFS dir */
-if(fs.exists(new Path(csvPath)))
-  fs.delete(new Path(csvPath),true)
+if (fs.exists(new Path(csvPath)))
+  fs.delete(new Path(csvPath), true)
 
 System.exit(0);
