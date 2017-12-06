cd0307b6d074fdebeee5f236c43c69741825d1f7
binbjz <binbjz@gmail.com>
2017-12-06 13:33:35 +0800

    update test cases

--- src/test/resources/spark-sql/common-sql/testAlterTableAddColumns.scala
+++ src/test/resources/spark-sql/common-sql/testAlterTableAddColumns.scala
@@ -1,7 +1,6 @@
 val test_sql="USE testdb";
 spark.sql(test_sql).collect().foreach(println);
 
-
 /*test Alter Table Add Columns*/
 
 val test_sql="ALTER TABLE tbl4addcolumns ADD COLUMNS (id STRING)";
