7a638bc94ee5fdc8e637cf92e612136bdedcb135
binbjz <binbjz@gmail.com>
2017-12-07 22:44:21 +0800

    sentry hive spark

--- src/test/java/DBAllWithEncryptedColumns.java
+++ src/test/java/DBAllWithEncryptedColumns.java
@@ -51,7 +51,7 @@ public class DBAllWithEncryptedColumns {
         //debug stage: write test results into output file.
         String output = sqlOutputPath + File.separator + resultFile;
         logger.info(output);
-        UtilTool.writeAllBytes(output, map.get(1).toString());
+        //UtilTool.writeAllBytes(output, map.get(1).toString());
 
         String expectedResults = UtilTool.readAllBytes(output);
         String actualResults = map.get(1).toString();
