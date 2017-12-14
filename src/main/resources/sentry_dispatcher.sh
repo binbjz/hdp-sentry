9b53c292c55609a3fc38584f3f58ec6ad4e298ca
binbjz <binbjz@gmail.com>
2017-12-14 10:35:31 +0800

    sentry hive spark

--- src/main/resources/sentry_dispatcher.sh
+++ src/main/resources/sentry_dispatcher.sh
@@ -25,6 +25,7 @@ libdir=${project_dir}/test_lib
 
 
 ##============
+: <<COMMENTBLOCK
 # Set hive env. If run hive sql please comment spark env statements
 hive_12=hive-1.2
 
@@ -32,9 +33,9 @@ HIVE_HOME=`readlink -f /opt/meituan/${hive_12}`
 cmd_exec="${HIVE_HOME}/bin/hive --hiveconf hive.cli.errors.ignore=true -f"
 sql_src=hive-sql
 file_suffix=sql
+COMMENTBLOCK
 
 # Set spark env. If run spark sql please comment hive env statements.
-: <<COMMENTBLOCK
 spark_21=spark-2.1
 spark_22=spark-2.2
 
@@ -42,7 +43,6 @@ SPARK_HOME=`readlink -f /opt/meituan/${spark_21}`
 cmd_exec="${SPARK_HOME}/bin/spark-shell --master yarn --deploy-mode client --queue root.hadoop-hdp.etltest --jars $project_dir/src/test/resources/source-data/hive_qa_udf.jar -i"
 sql_src=spark-sql
 file_suffix=scala
-COMMENTBLOCK
 ##============
 
 
@@ -86,7 +86,8 @@ source $project_dir/src/main/resources/sentry_super_env.sh setup SuperPrivil
 source $project_dir/src/main/resources/sentry_flag.sh
 
 # Run sentry test for standard authorization approach
-sentry_tcs="ServerAll ServerAlter ServerCreate ServerDrop ServerInsert ServerSelect ServerWrite DBAll DBAlter DBCreate DBDrop DBInsert DBSelect DBWrite TableAll TableAlter TableCreate TableDrop TableInsert TableSelect TableWrite DBAllWithEncryptedColumns DBAllWithEncryptedColumns_2 DBAllWithEncryptedColumns_3 GroupLogin GroupLogin_2 GroupLogin_3"
+#sentry_tcs="ServerAll ServerAlter ServerCreate ServerDrop ServerInsert ServerSelect ServerWrite DBAll DBAlter DBCreate DBDrop DBInsert DBSelect DBWrite TableAll TableAlter TableCreate TableDrop TableInsert TableSelect TableWrite DBAllWithEncryptedColumns DBAllWithEncryptedColumns_2 DBAllWithEncryptedColumns_3 GroupLogin GroupLogin_2 GroupLogin_3"
+sentry_tcs="DBAllWithEncryptedColumns DBAllWithEncryptedColumns_2 DBAllWithEncryptedColumns_3"
 
 for tc in $sentry_tcs; do
     # It will be used to set multiple permissions for the same test case
