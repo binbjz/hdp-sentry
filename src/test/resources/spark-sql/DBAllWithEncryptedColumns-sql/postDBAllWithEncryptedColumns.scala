2f06765d74c5cb665d8e12e8efa8d18b1c0fd1d8
binbjz <binbjz@gmail.com>
2017-12-15 21:11:59 +0800

    update test post sql

--- src/test/resources/spark-sql/DBAllWithEncryptedColumns-sql/postDBAllWithEncryptedColumns.scala
+++ src/test/resources/spark-sql/DBAllWithEncryptedColumns-sql/postDBAllWithEncryptedColumns.scala
@@ -12,7 +12,7 @@ val test_sql="DROP DATABASE encrypt_db4props";
 spark.sql(test_sql).collect().foreach(println);
 val test_sql="DROP DATABASE encrypt_db4tbl";
 spark.sql(test_sql).collect().foreach(println);
-val test_sql="DROP DATABASE encrypt_spark_testdb";
+val test_sql="DROP DATABASE encrypt_spark_testdb CASCADE";
 spark.sql(test_sql).collect().foreach(println);
 
 System.exit(0);
