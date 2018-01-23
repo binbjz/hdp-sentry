e2c18d57cd00c1dc797995c9c996c2de93ee5b34
binbjz <binbjz@gmail.com>
2018-01-23 19:08:41 +0800

    update sentry hive-1.2

--- src/test/java/DBInsert.java
+++ src/test/java/DBInsert.java
@@ -49,7 +49,7 @@ public class DBInsert {
 
         //debug stage: write test results into output file.
         String output = sqlOutputPath + File.separator + resultFile;
-        //UtilTool.writeAllBytes(output, map.get(1).toString());
+        UtilTool.writeAllBytes(output, map.get(1).toString());
 
         String expectedResults = UtilTool.readAllBytes(output);
         String actualResults = map.get(1).toString();
